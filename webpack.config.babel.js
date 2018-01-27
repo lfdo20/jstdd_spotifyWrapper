c0571b7ab93940a579d98ffc2bf8664eb8631294
Willian Justen <willianjustenqui@gmail.com>
2018-01-27 13:59:50 -0200

    Fix webpack build and minor things

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -3,7 +3,7 @@ import { join } from 'path';
 const include = join(__dirname, 'src');
 
 export default {
-  entry: './src/index.js',
+  entry: './src/index',
   output: {
     path: join(__dirname, 'dist'),
     libraryTarget: 'umd',
